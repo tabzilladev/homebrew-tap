c429677c0d07751f41e026fe34b22ac82afaa773
bdupras <brian@duprasville.com>
2026-06-12 14:33:35 -0600

    Fix deprecated depends_on macos comparison syntax (#1)
    * fix deprecated depends_on macos comparison syntax
    
    * split long lsregister path and trim desc to clear brew style
    
    ---------
    
    Co-authored-by: Brian Dupras <brian.dupras@airbnb.com>

--- Casks/tabzilla.rb
+++ Casks/tabzilla.rb
@@ -4,14 +4,16 @@ cask "tabzilla" do
 
   url "https://github.com/tabzilladev/tabzilla/releases/download/v#{version}/Tabzilla-#{version}-macos.zip"
   name "Tabzilla"
-  desc "URL routing daemon for macOS - routes links to browsers based on rules"
+  desc "URL routing daemon - routes links to browsers based on rules"
   homepage "https://github.com/tabzilladev/tabzilla"
 
-  depends_on macos: ">= :ventura"
+  depends_on macos: :ventura
+
   app "Tabzilla.app"
 
   postflight do
-    system_command "/System/Library/Frameworks/CoreServices.framework/Versions/Current/Frameworks/LaunchServices.framework/Versions/Current/Support/lsregister",
+    system_command "/System/Library/Frameworks/CoreServices.framework" \
+                   "/Frameworks/LaunchServices.framework/Support/lsregister",
                    args: ["-f", "#{appdir}/Tabzilla.app"]
   end
 
